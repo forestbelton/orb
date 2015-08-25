a86fb78412a854659cbe39a62f5b08daab265f89
centip3de <goodkingkyle@gmail.com>
2015-08-24 20:23:26 -0700

    Text rendering, yo

--- app/Main.hs
+++ app/Main.hs
@@ -7,7 +7,9 @@ import Foreign.C.Types
 import Foreign.C.String
 import Foreign.Marshal.Alloc
 import Foreign.Storable
+import Foreign (with) 
 import qualified Graphics.UI.SDL as SDL
+import qualified Graphics.UI.SDL.TTF as TTF
 import System.Exit
 
 data SDLContext = SDLContext {
@@ -26,12 +28,35 @@ initScreen width height = do
         SDL.setWindowTitle window s
     return $ SDLContext window renderer
 
+getWindow :: SDLContext -> SDL.Window
+getWindow (SDLContext window _) = window
+
+getRenderer :: SDLContext -> SDL.Renderer
+getRenderer (SDLContext _ renderer) = renderer
+
+arial :: String
+arial = "../assets/arial.ttf"
+
 main :: IO ()
 main = do
     ctx <- initScreen 800 600
     ptrEvent <- malloc
-    draw ctx
-    eventLoop ptrEvent ctx
+    --draw ctx
+    
+    TTF.withInit $ do
+        font <- TTF.openFont arial 150
+        textSurface <- TTF.renderUTF8Solid font "test" (SDL.Color 255 255 255 0)
+        textTexture <- SDL.createTextureFromSurface (getRenderer ctx) textSurface
+        loop ptrEvent ctx textTexture 
+
+loop :: Ptr SDL.Event -> SDLContext -> SDL.Texture -> IO ()
+loop event ctx textTexture = do
+    let loc = SDL.Rect 320 240 150 100
+    _ <- SDL.renderClear (getRenderer ctx) 
+    _ <- with loc $ \loc' ->
+             SDL.renderCopy (getRenderer ctx) textTexture nullPtr loc'
+    SDL.renderPresent (getRenderer ctx) 
+    eventLoop event ctx textTexture
 
 draw :: SDLContext -> IO ()
 draw (SDLContext w r) = do
@@ -41,12 +66,13 @@ draw (SDLContext w r) = do
     SDL.renderPresent r
     return ()
 
-eventLoop :: Ptr SDL.Event -> SDLContext -> IO ()
-eventLoop pe ctx = do
+eventLoop :: Ptr SDL.Event -> SDLContext -> SDL.Texture -> IO ()
+eventLoop pe ctx texture = do
     SDL.pollEvent pe
     ev <- peek pe
     case ev of
         SDL.QuitEvent _ _ -> exitSuccess
         _ -> do
-            draw ctx
-            eventLoop pe ctx
+            --draw ctx
+            loop pe ctx texture 
+            --eventLoop pe ctx texture
