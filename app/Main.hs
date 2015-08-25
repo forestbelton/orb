24323ef2eeb1f68d2f9e6df694254e68fd584b08
centip3de <goodkingkyle@gmail.com>
2015-08-25 14:22:49 -0700

    Added window/reference cleaning, moved text rendering to paintCmd, added new constructor for text rendering

--- app/Main.hs
+++ app/Main.hs
@@ -10,9 +10,7 @@ import Foreign.C.Types
 import Foreign.C.String
 import Foreign.Marshal.Alloc
 import Foreign.Storable
-import Foreign (with) 
 import qualified Graphics.UI.SDL as SDL
-import qualified Graphics.UI.SDL.TTF as TTF
 import qualified Data.Map as M
 import System.Exit
 
@@ -32,52 +30,36 @@ initScreen width height = do
         SDL.setWindowTitle window s
     return $ SDLContext window renderer
 
-getWindow :: SDLContext -> SDL.Window
-getWindow (SDLContext window _) = window
-
-getRenderer :: SDLContext -> SDL.Renderer
-getRenderer (SDLContext _ renderer) = renderer
-
-arial :: String
-arial = "../assets/arial.ttf"
+cleanUp :: SDLContext -> IO ()
+cleanUp (SDLContext win re) = do   
+    SDL.destroyRenderer re
+    SDL.destroyWindow win
+    SDL.quit
+    return ()
 
 main :: IO ()
 main = do
     ctx <- initScreen 800 600
     ptrEvent <- malloc
-    --draw ctx
-    
-    TTF.withInit $ do
-        font <- TTF.openFont arial 150
-        textSurface <- TTF.renderUTF8Solid font "test" (SDL.Color 255 255 255 1)
-        textTexture <- SDL.createTextureFromSurface (getRenderer ctx) textSurface
-        loop ptrEvent ctx textTexture 
-
-loop :: Ptr SDL.Event -> SDLContext -> SDL.Texture -> IO ()
-loop event ctx textTexture = do
-    let loc = SDL.Rect 320 240 150 100
-    SDL.renderClear (getRenderer ctx) 
-    with loc $ \loc' ->
-             SDL.renderCopy (getRenderer ctx) textTexture nullPtr loc'
-    SDL.renderPresent (getRenderer ctx) 
-    eventLoop event ctx textTexture
+    draw ctx
+    eventLoop ptrEvent ctx
+    cleanUp ctx
 
 draw :: SDLContext -> IO ()
 draw (SDLContext w r) = do
-    SDL.setRenderDrawColor r 255 255 255 255
-    SDL.renderClear r
+    --SDL.setRenderDrawColor r 255 255 255 255
+    --SDL.renderClear r
     let dim = Dimensions (SDL.Rect 0 0 800 50) noEdges noEdges noEdges
     paint r $ buildDisplayCommands $ Node (dim, Block, snd $ parseStyle "div { background-color: blue; }") []
     SDL.renderPresent r
     return ()
 
-eventLoop :: Ptr SDL.Event -> SDLContext -> SDL.Texture -> IO ()
-eventLoop pe ctx texture = do
+eventLoop :: Ptr SDL.Event -> SDLContext -> IO ()
+eventLoop pe ctx = do
     SDL.pollEvent pe
     ev <- peek pe
     case ev of
         SDL.QuitEvent _ _ -> exitSuccess
         _ -> do
-            --draw ctx
-            loop pe ctx texture 
-            --eventLoop pe ctx texture
+            draw ctx
+            eventLoop pe ctx 
--- src/Paint.hs
+++ src/Paint.hs
@@ -3,13 +3,19 @@ module Paint (DisplayCommand(..), paint) where
 import Foreign.Marshal.Alloc
 import Foreign.Storable
 import Foreign.Ptr
+import Foreign (with)
 
 import System.IO
 
 import qualified Graphics.UI.SDL as SDL
+import qualified Graphics.UI.SDL.TTF as TTF
+
 
 data DisplayCommand =
-    SolidColor SDL.Color SDL.Rect
+    SolidColor SDL.Color SDL.Rect | FontData (Int, Int) String Int SDL.Color String
+
+arial :: String
+arial = "../assets/arial.ttf"
 
 paintCmd :: SDL.Renderer -> DisplayCommand -> IO ()
 paintCmd re cmd = case cmd of
@@ -19,6 +25,16 @@ paintCmd re cmd = case cmd of
             SDL.setRenderDrawColor re r g b a
             SDL.renderFillRect re ptrRect
             return ()
+    FontData (x, y) name weight color text -> do
+         TTF.withInit $ do
+            font <- TTF.openFont arial 150
+            textSurface <- TTF.renderUTF8Solid font "test" (SDL.Color 255 255 255 1)
+            textTexture <- SDL.createTextureFromSurface re textSurface
+            let loc = SDL.Rect 320 240 150 100
+            with loc $ \loc' ->
+             SDL.renderCopy re textTexture nullPtr loc'
+            return ()
+     
 
 paint :: SDL.Renderer -> [DisplayCommand] -> IO ()
 paint re = mapM_ (paintCmd re)
